aeba7a1bc9cd446c4cb433486cc872951ebfb763
MasterCheapster <84666759+MasterCheapster@users.noreply.github.com>
2023-12-14 19:49:50 +0530

    New app

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGuJYQVW6-fWUh06UmYWAjeUI3Y6RYNak",
+  authDomain: "shop-4af1a.firebaseapp.com",
+  projectId: "shop-4af1a",
+  storageBucket: "shop-4af1a.appspot.com",
+  messagingSenderId: "32822490870",
+  appId: "1:32822490870:web:b75e2945e81d7f2bb75ed7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ client/src/data.js
@@ -0,0 +1,62 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://img.mensxp.com/media/content/2023/May/Main-Image---iStock_6468782f3daca.jpeg?w=750&h=625&cc=1",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT DISCOUNTS ON NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "NEW SUNGLASSES",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT DISCOUNTS ON NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "TRY CONTACT LENS",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT DISCOUNTS ON NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/10536608/pexels-photo-10536608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "PERFUMES!",
+    cat: "Perfumes",
+  },
+  {
+    id: 2,
+    img: "https://m5z7q7e5.rocketcdn.me/wp-content/uploads/2018/06/contact-lenses-model-1030x684.jpg",
+    title: "BRANDED CONTACT LENS",
+    cat: "Contact Lens",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5794271/pexels-photo-5794271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "SUNGLASSES",
+    cat: "Sunglasses",
+  },
+  {
+    id: 4,
+    img: "https://colors.myalcon.com/sites/g/files/rbvwei666/files/2019-05/Colors_Air-Optix-Colors_2x_Brilliant-Blue%402x-n.png",
+    title: "COLORED CONTACT LENS",
+    cat: "Colored Lens",
+  },
+  {
+    id: 5,
+    img: "https://5.imimg.com/data5/ECOM/Default/2022/8/TT/VP/CZ/24103511/lensclenaserimagemultipurpose-4c1f1c8d-2390-4375-939d-12d9b30937a8-1000x1000.jpg",
+    title: "LENS CLEANER",
+    cat: "Cleaner",
+  },
+  {
+    id: 6,
+    img: "https://images.pexels.com/photos/1123256/pexels-photo-1123256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "CANDLES",
+    cat: "Candle",
+  },
+];
